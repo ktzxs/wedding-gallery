c0e074827a8befe3b192b3b3ed24129167c9aed0
ktzxs <lucasalvesdacosta070@gmail.com>
2026-08-09 19:04:15 -0300

    feat(api): feito algumas config da api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+
+  app.enableCors({
+    origin: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  const PORT = process.env.PORT ?? 4000
+  await app.listen(PORT)
+  console.log(`Application is running in http://localhost:${PORT}/api`);
 }
 bootstrap();
